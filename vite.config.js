2254bf4cd44e6d9fc7537f9f6acec6ea96b1e3d9
Ryan Young <ryanyatwork@gmail.com>
2025-12-25 12:56:19 -0800

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/annikas-dish-rentals/",
+});
